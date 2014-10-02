7ad7322cea535a26daea319124ee25b53e331e68
drim06 <marc.destefanis.etu@gmail.com>
2014-10-02 18:53:44 +0200

    stable/ last version/ asteroides losange

--- spaceshipsVsAsteroids/Asteroid.cpp
+++ spaceshipsVsAsteroids/Asteroid.cpp
@@ -1,6 +1,6 @@
 #include "Asteroid.h"
 
-void Asteroid::draw(){
+/*void Asteroid::draw(){
 	GraphicPrimitives::drawFillTriangle2D(
 		posX_,
 		posY_,
@@ -8,6 +8,34 @@ void Asteroid::draw(){
 		posY_ -0.02f,
 		posX_ -0.05f,
 		posY_ + 0.02f,
+		red_,
+		green_,
+		blue_);
+}*/
+
+void Asteroid::draw(){
+	std::vector<float> x;
+	std::vector<float> y;
+
+	// x values
+	x.push_back(posX_);
+	x.push_back(posX_ + 0.035f);
+	x.push_back(posX_ + 0.07f);
+	x.push_back(posX_ + 0.035f);
+	x.push_back(posX_ + 0.14f);
+	x.push_back(posX_ + 0.07f);
+
+	// y values
+	y.push_back(posY_);
+	y.push_back(posY_ + 0.035f);
+	y.push_back(posY_ + 0.07f);
+	y.push_back(posY_ + 0.035f);
+	y.push_back(posY_);
+	y.push_back(posY_-0.07f);
+
+	GraphicPrimitives::drawFillPolygone2D(
+		x,
+		y,
 		red_,
 		green_,
 		blue_);
